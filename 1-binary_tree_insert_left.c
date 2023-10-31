41665981cbc565cb933cfb7c1f4ad1e33f4a7c2b
Azizaazu <aynaou.aziza98@gmail.com>
2023-10-31 16:12:49 -0700

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,7 +25,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
+	}
 
 	parent->left = new_node;
 
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -24,7 +24,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->right = parent->right;
 
 	if (parent->right != NULL)
+	{
+		new_node->left = parent->left;
 		parent->right->parent = new_node;
+	}
 
 	parent->right = new_node;
 
