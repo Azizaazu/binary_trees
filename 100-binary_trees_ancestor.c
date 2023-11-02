db9b5c9b0a7c52b79bf47bcd9ffc86974db65639
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-02 14:50:09 -0700

    update

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 		return ((binary_tree_t *)first);
 
 	m = first->parent, p = second->parent;
+
 	if (first == p || !m || (!m->parent && p))
 		return (binary_trees_ancestor(first, p));
 	else if (m == second || !p || (!p->parent && m))
