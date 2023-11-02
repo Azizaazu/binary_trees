f9bedaca45677db895be03bb4001be23f4e1e909
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-02 10:14:50 -0700

    update

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,13 +11,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right == NULL) || (tree->left != NULL && tree->right != NULL))
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
 	{
 		int left_full = binary_tree_is_full(tree->left);
 		int right_full = binary_tree_is_full(tree->right);
 
 		return (left_full && right_full);
 	}
-	else
-		return (0);
+	return (0);
 }
