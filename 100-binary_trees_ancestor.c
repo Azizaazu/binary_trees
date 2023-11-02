ab4102ba7adceeea7d13bf049fc1fb66a813d3ff
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-02 13:52:32 -0700

    update

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * is_ancestor - Test ancestor function and print informations
+ * @node: node
+ * @ancestor: pointer
+ * Return: 0
+ */
+
+int is_ancestor(const binary_tree_t *ancestor, const binary_tree_t *node)
+{
+	while (node != NULL)
+		if (node == ancestor)
+			return (1);
+
+	node = node->parent;
+
+	return (0);
+}
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ *
+ * @first: first node
+ * @second: Second node
+ * Return: return pointer or NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *ancestor = first;
+
+	while (ancestor != NULL)
+	{
+		if (is_ancestor(ancestor, second))
+			return ((binary_tree_t *)ancestor);
+
+	ancestor = ancestor->parent;
+	}
+	return (NULL);
+}
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,10 +11,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-		return (parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 	else
-		return (parent->left);
+		return (node->parent->left);
 }
