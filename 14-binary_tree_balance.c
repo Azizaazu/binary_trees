034d1af2c5e0339df686d62145cdb970bebf6f73
Azizaazu <aynaou.aziza98@gmail.com>
2023-11-02 11:03:18 -0700

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,12 +26,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
 
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height) ? (left_height + 1)
 			: (right_height + 1));
+	}
+	return (0);
 }
